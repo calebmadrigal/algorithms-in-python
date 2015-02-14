28d1f8bdf1aef7943bbcf524699b784881a8f741
Caleb Madrigal <caleb.madrigal@gmail.com>
2015-02-13 23:19:19 -0600

    Updated to python3 and added some metadata

--- mergesort.py
+++ mergesort.py
@@ -1,11 +1,16 @@
+"""mergesort.py - implementation of mergesort. """
+
+__author__ = "Caleb Madrigal"
+__date__ = "2015-02-13"
+
 def merge(left, right):
     if len(left) < 1:
         return right
     elif len(right) < 1:
         return left
     else:
-        (first_left, rest_left) = (left[0], left[1:])
-        (first_right, rest_right) = (right[0], right[1:])
+        first_left, *rest_left = left
+        first_right, *rest_right = right
         if first_left <= first_right:
             return [first_left] + merge(rest_left, right)
         else:
@@ -21,7 +26,7 @@ def mergesort(lst):
         else:
             return [lst[1], lst[0]]
     else: # list_len > 2
-        split_point = list_len / 2
+        split_point = list_len // 2
         left = lst[0:split_point]
         right = lst[split_point:]
         left_sorted = mergesort(left)
@@ -31,6 +36,6 @@ def mergesort(lst):
 if __name__ == "__main__":
     import random
     data = [random.randint(0,10) for i in range(9)]
-    print 'UNSORTED:', data
-    print 'SORTED:  ', mergesort(data)
+    print('UNSORTED:', data)
+    print('SORTED:  ', mergesort(data))
 
